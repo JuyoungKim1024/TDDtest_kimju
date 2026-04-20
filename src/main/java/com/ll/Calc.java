c290f1405cadd10660b0d10eee74758c95912a65
Kim Juyoung <op45672@gmail.com>
2026-04-21 08:00:18 +0900

    ()괄호연산 테스트

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,6 +2,17 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
+        while (expression.contains("(")) {
+            int openIndex = expression.lastIndexOf("(");
+            int closeIndex = expression.indexOf(")", openIndex);
+
+            String innerExpression = expression.substring(openIndex + 1, closeIndex);
+            int innerResult = run(innerExpression);
+
+            expression = expression.substring(0, openIndex)
+                    + innerResult
+                    + expression.substring(closeIndex + 1);
+        }
         String[] bits = expression.split(" ");
         int result = 0;
         int num = Integer.parseInt(bits[0]);
@@ -21,7 +32,6 @@ public class Calc {
                 num = Integer.parseInt(bits[i + 1]);
             }
         }
-
         if (sign.equals("+")) {
             result += num;
         } else if (sign.equals("-")) {
