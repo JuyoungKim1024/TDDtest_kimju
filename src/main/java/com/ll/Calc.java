338de807b5d0fd0094f833937c8ec4e457f30ff0
Kim Juyoung <op45672@gmail.com>
2026-04-21 07:56:11 +0900

    t15~17 곱하기 포함 다항식 연산

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,20 +3,29 @@ package com.ll;
 public class Calc {
     public static int run(String expression) {
         String[] bits = expression.split(" ");
-
-        int result = Integer.parseInt(bits[0]);
+        int result = 0;
+        int num = Integer.parseInt(bits[0]);
+        String sign = "+";
 
         for (int i = 1; i < bits.length; i += 2) {
+            if (bits[i].equals("*")) {
+                num *= Integer.parseInt(bits[i + 1]);
+            } else {
+                if (sign.equals("+")) {
+                    result += num;
+                } else if (sign.equals("-")) {
+                    result -= num;
+                }
 
-            int num = Integer.parseInt(bits[i + 1]);
-
-            if (bits[i].equals("+")) {
-                result += num;
-            } else if (bits[i].equals("-")) {
-                result -= num;
-            }else if (bits[i].equals("*")) {
-                result *= num;
+                sign = bits[i];
+                num = Integer.parseInt(bits[i + 1]);
             }
+        }
+
+        if (sign.equals("+")) {
+            result += num;
+        } else if (sign.equals("-")) {
+            result -= num;
         }
 
         return result;
